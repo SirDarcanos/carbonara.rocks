0b9dac9a1c420e1489aba7d35601ae59afc059ba
Nicola Mustone <nicola@mustone.blog>
2026-06-26 07:32:04 +0300

    Project scaffolding

---
+++ eslint.config.ts
@@ -0,0 +1,10 @@
+import js from "@eslint/js";
+import ts from "typescript-eslint";
+import astro from "eslint-plugin-astro";
+
+export default [
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  ...astro.configs.recommended,
+  { ignores: ["dist/", ".astro/"] },
+];
